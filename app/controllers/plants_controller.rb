6c2673b9472a900d0f50cceb4fe6faea82a48c2c
philipdemand <117778804+philipdemand@users.noreply.github.com>
2023-08-29 15:02:14 -0400

    commit tree 2cb3f2a65d40beb5dcf28d6afb486a11b3c7e0f4

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -18,9 +18,36 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update
+    plant = find_plant
+    plant.update(plant_params)
+    render json: plant
+  rescue ActiveRecord::RecordNotFound
+      render_not_found_response
+  end
+
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.destroy
+      head :no_content
+    else
+      render json: { error: "Plant not found" }, status: :not_found
+    end
+  end
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
+
+  def render_not_found_response
+    render json: { error: "Plant not found" }, status: :not_found
+  end
+
+  def find_plant
+    Plant.find(params[:id])
+  end
+
 end
